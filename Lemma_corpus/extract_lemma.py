cb9098278e2015f2c350bb1f0e35e99c4de93270
Gábor Palkó <luhpeg2@gmail.com>
2023-02-17 19:28:33 +0100

    Next commit

--- Lemma_corpus/extract_lemma.py
+++ Lemma_corpus/extract_lemma.py
@@ -1,6 +1,8 @@
 import os
 from bs4 import BeautifulSoup
-import  time
+import time
+import lxml
+from multiprocessing import Pool
 
 
 def file_list(path_in):
@@ -14,23 +16,31 @@ def file_list(path_in):
 
 
 def write_corpus(file_path):
+    begin = time.time()
     l_list = lemma_list(file_path)
     with open("corpus.cor", "a", encoding="utf8") as f_cor:
-
-
-
-
+        print(" ".join(l_list), file=f_cor)
+        end = time.time()
+        print(file_path, end-begin)
 
 
 def lemma_list(path):
-    soup = BeautifulSoup(path, "xml")
-    lem_list = [w["lemma"] for w in soup.find_all("w")]
-    return lem_list
-
-
+    with open(path, "r", encoding="utf8") as f_xml:
+        soup = BeautifulSoup(f_xml, "xml")
+        lem_list = [w["lemma"] for w in soup.find_all("w")]
+        return lem_list
 
 
 if __name__ == '__main__':
-    with open("corpus.cor", "w", encoding="utf8") as f:
+    actual_path = input("Path of the files to process?\nDefault: /home/eltedh/PycharmProjects/DATA\n")
+    if actual_path.replace(" ", "") == "":
         actual_path = "/home/eltedh/PycharmProjects/DATA"
-
+    overwrite = input("Add new lemmas to corpus(a) or start new (w)? Default: start new.\n")
+    if overwrite == "a":
+        pass
+    else:
+        overwrite = "w"
+    with open("corpus.cor", overwrite, encoding="utf8") as f:
+        files = file_list(actual_path)
+        with Pool() as p:
+            p.map(write_corpus, files)
