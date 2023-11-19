9be0c71bf63c81b7547b2f3ffa585549efb33a01
Gábor Palkó <luhpeg2@gmail.com>
2023-11-19 12:54:15 +0100

    Next commit

--- KOHA/koha_dump_2_tsv.py
+++ KOHA/koha_dump_2_tsv.py
@@ -1,25 +1,43 @@
 from bs4 import BeautifulSoup
 
+def extract_field_value(soup, tag, subfield_code, tag_value):
+    """
+    Extract the value of a field (controlfield or datafield) from BeautifulSoup object.
+    """
+    field = "Unknown"
+    if tag == "controlfield":
+        field = soup.find(tag, {'tag': tag_value}).text.strip().replace('  ', '')
+    else:
+        if (soup.find(tag, {'tag': tag_value})
+                and soup.find(tag, {'tag': tag_value}).find('subfield', {'code': subfield_code})):
+            field = soup.find(tag, {'tag': tag_value}).find('subfield', {'code': subfield_code}).text.strip()
+
+    return field
+
 
 def parse_large_txt_file(input_file_path, output_tsv_path):
-    # Open the TSV file for writing
+    # Define datafields to extract
+    datafields = [
+        {'tag': 'controlfield', 'subfield_code': '', 'tag_value': '001'},
+        {'tag': 'controlfield', 'subfield_code': '', 'tag_value': '003'},
+        {'tag': 'datafield', 'subfield_code': 'a', 'tag_value': '010'},
+        {'tag': 'datafield', 'subfield_code': 'a', 'tag_value': '100'},
+        {'tag': 'datafield', 'subfield_code': 'd', 'tag_value': '100'},
+        {'tag': 'datafield', 'subfield_code': 'a', 'tag_value': '667'},
+        {'tag': 'datafield', 'subfield_code': 'u', 'tag_value': '856'},
+        {'tag': 'datafield', 'subfield_code': 'a', 'tag_value': '902'},
+        {'tag': 'datafield', 'subfield_code': 'a', 'tag_value': '906'}
+    ]
+
     with open(output_tsv_path, 'w', encoding='utf-8') as tsv_file:
         # Write header to the TSV file
-        tsv_file.write("Controlfield_001\t"
-                       "Controlfield_003\t"
-                       "Datafield_010\t"
-                       "Datafield_100a\t"
-                       "Datafield_100d}\t"
-                       "Datafield_856u\t"
-                       "Datafield_902a\t"
-                       "Datafield_906a\n")
+        header = '\t'.join([f"{field['tag']}_{field['tag_value']}{field['subfield_code']}" for field in datafields]) + '\n'
+        tsv_file.write(header)
 
-        # Open the large text file for reading line by line
         with open(input_file_path, 'r', encoding='utf-8') as file:
             in_record_segment = False
             record_segment = ""
 
-            # Process each line in the file
             for line in file:
                 if '<record>' in line:
                     in_record_segment = True
@@ -28,59 +46,23 @@ def parse_large_txt_file(input_file_path, output_tsv_path):
                     in_record_segment = False
                     record_segment += line
 
-                    # Check if the segment contains the specified subfield
                     if '<subfield code="a">PERSO_NAME</subfield>' in record_segment:
-                        # Parse the segment with BeautifulSoup
                         soup = BeautifulSoup(record_segment, 'xml')
 
-                        # Extract values of controlfields
-                        controlfield_001 = soup.find('controlfield',
-                                                     {'tag': '001'}).text.strip().replace(' ', '')
-                        controlfield_003 = soup.find('controlfield', {'tag': '003'}).text.strip()
-
-                        # Extract the values of datafields
-                        if soup.find('datafield', {'tag': '010'}):
-                            datafield_010 = soup.find('datafield',{'tag': '010'}).find('subfield',
-                                                    {'code': 'a'}).text.strip().replace(' ', '')
-                        else:
-                            datafield_010 = 'Unknown'
-
-                        if soup.find('datafield', {'tag': '100'}).find('subfield', {'code': 'a'}):
-                            datafield_100a = soup.find('datafield', {'tag': '100'}).find('subfield',
-                                                                                     {'code': 'a'}).text.strip()
-                        else:
-                            datafield_100a = 'Unknown'
-                        if soup.find('datafield', {'tag': '100'}).find('subfield', {'code': 'd'}):
-                            datafield_100d = soup.find('datafield', {'tag': '100'}).find('subfield',
-                                                                                     {'code': 'd'}).text.strip()
-                        else:
-                            datafield_100d = 'Unknown'
-                        if soup.find('datafield', {'tag': '856'}):
-                            datafield_856u = soup.find('datafield', {'tag': '856'}).find('subfield',
-                                                                                         {'code': 'u'}).text.strip()
-                        else:
-                            datafield_856u = 'Unknown'
-
-                        if soup.find('datafield', {'tag': '902'}):
-                            datafield_902a = soup.find('datafield', {'tag': '902'}).find('subfield',
-                                                                                         {'code': 'a'}).text.strip()
-                        else:
-                            datafield_902a = 'Unknown'
-                        if soup.find('datafield', {'tag': '906'}):
-                            datafield_906a = soup.find('datafield', {'tag': '906'}).find('subfield',
-                                                                                         {'code': 'a'}).text.strip()
-                        else:
-                            datafield_906a = 'Unknown'
+                        # Extract values of datafields
+                        datafield_values = [
+                            extract_field_value(soup, field['tag'], field.get('subfield_code'), field.get('tag_value'))
+                            for field in datafields
+                        ]
 
                         # Write values to the TSV file
-                        tsv_file.write(f"{controlfield_001}\t{controlfield_003}\t{datafield_010}\t{datafield_100a}"
-                                       f"\t{datafield_100d}\t{datafield_856u}\t{datafield_902a}\t{datafield_906a}\n")
+                        tsv_file.write('\t'.join(datafield_values) + '\n')
                 elif in_record_segment:
                     record_segment += line
 
 
 # Example usage
-input_file_path = '/home/eltedh/PycharmProjects/DATA/KOHA/auths.txt'
+input_file_path = '/home/eltedh/PycharmProjects/DATA/KOHA/koha_auth_part.txt'
 output_tsv_path = 'koha_pim.tsv'
 
 parse_large_txt_file(input_file_path, output_tsv_path)
