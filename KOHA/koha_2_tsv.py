7061e1229e701bca63a2e0e3ddea2833d1d87f51
Gábor Palkó <luhpeg2@gmail.com>
2023-11-19 15:20:01 +0100

    Next commit

--- KOHA/koha_2_tsv.py
+++ KOHA/koha_2_tsv.py
@@ -51,15 +51,20 @@ def create_dictionary(f_path):
 
     koha_id_dict = defaultdict(set)
     names_without_koha_id = set()
+    viaf_id_dict = defaultdict(set)
+    names_without_viaf_id = set()
+
     for root, dirs, files in os.walk(f_path):
         for filename in files:
             if filename.endswith(".xml"):
                 xml_path = os.path.join(root, filename)
                 parsed_xml = parse_xml(xml_path)
 
+                # Extracting KOHA authorities
                 for idno_tag in parsed_xml.find_all('idno', {'type': 'KOHA_AUTH'}):
                     if idno_tag.text != "":
-                        koha_id = idno_tag.text.replace("KOHA_AUTH:", "").replace("KOHA:", "")
+                        koha_id = (idno_tag.text.replace("KOHA_AUTH:", "")
+                                   .replace("KOHA:", "").strip())
                         corresp_name = idno_tag.get('corresp')
                         if corresp_name:
                             koha_id_dict[koha_id].add(corresp_name.strip())
@@ -70,10 +75,25 @@ def create_dictionary(f_path):
                         if corresp_name:
                             names_without_koha_id.add(corresp_name.strip())
 
-            write_to_tsv(koha_id_dict, names_without_koha_id)
+                # Extracting VIAF authorities
+                for idno_tag in parsed_xml.find_all('idno', {'type': 'VIAF_AUTH'}):
+                    if idno_tag.text != "":
+                        viaf_id = (idno_tag.text.replace("VIAF_AUTH:", "")
+                                   .replace("VIAF:", "").strip())
+                        corresp_name = idno_tag.get('corresp').strip()
+                        if corresp_name:
+                            viaf_id_dict[viaf_id].add(corresp_name.strip())
+                        else:
+                            viaf_id_dict[koha_id].add("")
+                    else:
+                        corresp_name = idno_tag.get('corresp')
+                        if corresp_name:
+                            names_without_viaf_id.add(corresp_name.strip())
+
+            write_to_tsv(koha_id_dict, names_without_koha_id, viaf_id_dict, names_without_viaf_id)
 
 
-def write_to_tsv(koha_dict, names_set):
+def write_to_tsv(koha_dict, koha_names_set, viaf_dict, viaf_names_set):
     """
     Writes the values of the dictionary into a single line of the TSV.
 
@@ -87,7 +107,17 @@ def write_to_tsv(koha_dict, names_set):
             tsv_file.write(key + '\t' + "; ".join(value) + '\n')
 
     with open('names_without_Koha_id', 'w', encoding='utf-8') as tsv_file:
-        for i in sorted(names_set):
+        for i in sorted(koha_names_set):
+            tsv_file.write(i + '\n')
+
+    with open('viaf_id_list.tsv', 'w', encoding='utf-8') as tsv_file:
+        for key in viaf_dict.keys():
+            value = viaf_dict[key]
+            value.discard("")
+            tsv_file.write(key + '\t' + "; ".join(value) + '\n')
+
+    with open('names_without_viaf_id', 'w', encoding='utf-8') as tsv_file:
+        for i in sorted(viaf_names_set):
             tsv_file.write(i + '\n')
 
 
