db885dfca11a15d6842e1e155c4c2a2d04dc6d72
Gábor Palkó <luhpeg2@gmail.com>
2024-01-18 18:11:19 +0100

    Next commit

--- AJOM17_18_19/ad_hoc_search.py
+++ AJOM17_18_19/ad_hoc_search.py
@@ -6,7 +6,10 @@ from xml_methods import get_filenames
 
 folder_list = ["/home/eltedh/PycharmProjects/XML-processing/AJOM17_18_19/AJOM_GEO/XML"]
 
+placename_header_note = set()
 for parsed, path in get_filenames(folder_list):
-    for idno in parsed.teiHeader.find_all("idno", {"type": "KOHA_GEO"}):
-        if idno.parent.parent.name != "p":
-            print(idno, idno.parent.name, idno.parent.parent.name, path)
+    for place in parsed.teiHeader.find_all("placeName"):
+        if place.find("idno"):
+            if place.idno.get("corresp"):
+                placename_header_note.add(place.idno["corresp"])
+print(placename_header_note)
