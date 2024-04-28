67f1b555d44f7051e683d1004c44a0b9ddc1d5ff
Gábor Palkó <palko.gabor@btk.elte.hu>
2024-04-28 19:44:12 +0200

    Next commit

--- InvenioRDM/File_uploader/file_uploader.py
+++ InvenioRDM/File_uploader/file_uploader.py
@@ -31,21 +31,31 @@ def main(records, token):
         # with open(datafile) as fp:
         #     data = json.load(fp)
 
-        # # Create the record
-        # # note: "verify=False" is so that we can connect to 127.0.0.1 with a
-        # # self-signed certificate. You should not do this in production.
-        # r = requests.post(
-        #     f"{api}/api/records", data=json.dumps(data), headers=h, verify=False)
-        # assert r.status_code == 201, \
-        #     f"Failed to create record (code: {r.status_code})"
-        # links = r.json()['links']
-        links_files = f"https://invenio.abtk.hu/api/records/{invenio_key}/files"
+        # # Create a new version
+        r = requests.post(
+            f"{api}/api/records/{invenio_key}/versions", headers=h, verify=False)
+        assert r.status_code == 201, \
+            f"Failed to create record (code: {r.status_code})"
+        new_version_json = r.json()
+        new_key = new_version_json["links"]["self"].split("/")[-2]
+        print(new_version_json)
+        links = r.json()['links']
+        print(new_key)
+
+        # # Update publication date and version
+        # data =
+        # r = requests.put(
+        #                 f"{api}/api/records/{new_key}/draft", data=data, headers=h, verify=False)
+        # print(r.json)
+        # assert r.status_code == 200, \
+        #     f"Failed to update date (code: {r.status_code})"
 
         # Upload files
         for f in files:
             # Initiate the file
             data = json.dumps([{"key": f}])
-            r = requests.post(links_files, data=data, headers=h, verify=False)
+            print(data, "\n", links["files"])
+            r = requests.post(links["files"], data=data, headers=h, verify=False)
             assert r.status_code == 201, \
                 f"Failed to create file {f} (code: {r.status_code})"
             file_links = r.json()["entries"][0]["links"]
@@ -71,8 +81,8 @@ def main(records, token):
 if __name__ == '__main__':
     path = "/home/pg/Documents/GitHub/XML-processing/InvenioRDM/File_uploader"
     invenio_key_and_file = [
-                            ("vxd8n-wqf60", ["rmkt-17-6.tei.eloszo", ])
+                            ("vxd8n-wqf60", ["rmkt-17-6.tei.eloszo.xml", ])
                             ]
-    with open("/InvenioRDM/token.txt", "r", encoding="utf8") as f:
+    with open("/home/pg/Documents/GitHub/XML-processing/InvenioRDM/Uploader/token.txt", "r", encoding="utf8") as f:
         invenio_token = f.read().strip()
         main(invenio_key_and_file, invenio_token)
